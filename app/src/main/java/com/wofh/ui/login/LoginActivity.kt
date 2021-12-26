63e238be83fa578f70c214c95440c3e3c41872ec
Ubean <ubeannn@gmail.com>
2021-12-26 15:19:09 +0700

    UI Login and Register

---
+++ app/src/main/java/com/wofh/ui/login/LoginActivity.kt
@@ -0,0 +1,21 @@
+package com.wofh.ui.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.wofh.databinding.ActivityLoginBinding
+
+class LoginActivity : AppCompatActivity() {
+    private var _binding: ActivityLoginBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        _binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+}
